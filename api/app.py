17859363f9865069da6de5eae86a3c99e9d35fc5
MattToast <matthew.drozt@gmail.com>
2021-03-25 06:54:20 -0400

    reconfig with fask documentation

--- api/app.py
+++ api/app.py
@@ -3,10 +3,13 @@ from flask import Flask, json, jsonify
 app = Flask(__name__, static_folder="../build", static_url_path="/")
 
 
-@app.route('/')
-def index():
-    return app.send_static_file("index.html")
 
 @app.route('/api/message')
 def hello_world():
     return jsonify({"message": "hello world"})
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return app.send_static_file("index.html")
