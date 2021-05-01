af2be2409368ec0773b7a42125d279bc67569ec0
MattToast <matthew.drozt@gmail.com>
2021-05-01 18:37:23 -0400

    Minor changes to reports.py for security

--- api/routes/reports.py
+++ api/routes/reports.py
@@ -3,12 +3,12 @@ from flask import Blueprint
 reportRoutes = Blueprint('reportRoutes', __name__)
 
 
-@reportRoutes.route('/transfers')
+@reportRoutes.route('/transfers', methods=["GET"])
 def transfers():
     return "Get Transfer Report"
 
 
-@reportRoutes.route('empsales/<empID>')
-def employeeSales(empID):
+@reportRoutes.route('empsales/<empID>', methods=["GET"])
+def employee_sales(empID):
     return f"Get employee sales for employee with id {empID}"
 
